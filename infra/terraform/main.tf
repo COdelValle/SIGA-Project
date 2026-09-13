ca5bcfdfcaec868a971b32ec4d9a17a7ddef6f71
Llaitureo <fe.llaitureo@duocuc.cl>
2026-09-13 17:51:49 -0300

    Hotfix | main.tf
    no había un proveedor.

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_instance" "ec2_frontend" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
